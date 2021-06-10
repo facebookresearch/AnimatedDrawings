1c7d161de1e4ec69cbe5c2f89c88fbb4dfdc5b55
Somya Jain <somyaj@fb.com>
2021-06-10 03:05:25 -0400

    Created a home page with a background video and upload button

--- ui/www/src/App.tsx
+++ ui/www/src/App.tsx
@@ -1,25 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Button,
+  Carousel,
+  Col,
+  Container,
+  Form,
+  Modal,
+  Row,
+} from "react-bootstrap";
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Carousel fade>
+        <Carousel.Item interval={7000}>
+          <video playsInline autoPlay muted loop id="">
+            <source src="demo1.mp4" type="video/mp4" />
+          </video>
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={7000}>
+          <video playsInline autoPlay muted loop poster="polina.jpg" id="">
+            <source src="demo2.mp4" type="video/mp4" />
+          </video>
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <Container fluid>
+        <Row noGutters={true} className="h-100">
+          <Col className="align-self-center h-100">
+            <Button
+              variant="primary"
+              size="lg"
+              onClick={handleShow}
+              className="align-self-center"
+            >
+              Create your own Animations
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Create your own animation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Alright! Let's get started by uploading an image of drawing!
+          <Form className="mt-3">
+            <Form.File
+              id="custom-file"
+              label="Custom file input"
+              custom
+              onChange={(e: React.ChangeEvent) => {
+                // doSomethingWithFiles(e.target.files)
+              }}
+            />
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
--- ui/www/src/index.tsx
+++ ui/www/src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
