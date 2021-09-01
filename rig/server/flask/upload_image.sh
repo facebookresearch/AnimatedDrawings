b0ada8b748a5064a6955271619e9905a3e99df15
Jesse Smith <hjessmith@devfair0109.h2.fair>
2021-09-01 09:37:43 -0700

    added upload_image.sh

---
+++ rig/server/flask/upload_image.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+set -e
+set -x
+
+UPLOAD_FN=${1}
+UUID=${2}
+
+OUTPUT_PARENT_DIR='/private/home/hjessmith/flask/output_predictions/'${UUID}
+mkdir -p ${OUTPUT_PARENT_DIR}
+mv uploads/${UPLOAD_FN} ${OUTPUT_PARENT_DIR}/image.png
