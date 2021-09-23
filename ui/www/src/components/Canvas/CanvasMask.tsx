66a0ec0b0e1cae1c26627c9bd9b382ee32f87577
Sahir Gomez <sahir@fb.com>
2021-09-22 21:00:19 -0400

    added a get joint location call

--- ui/www/src/components/Canvas/CanvasMask.tsx
+++ ui/www/src/components/Canvas/CanvasMask.tsx
@@ -7,6 +7,93 @@ import useStepperStore from "../../hooks/useStepperStore";
 import { useDrawingApi } from "../../hooks/useDrawingApi";
 import Loader from "../Loader";
 import MaskStage from "./MaskStage";
+import { Position } from "../PoseEditor";
+
+const mapJointsToPose = (joints: object) => {
+  return {
+    nodes: Object.entries(joints).map((arr) => {
+      return { id: arr[0], label: arr[0], position: arr[1] as Position };
+    }),
+    edges: [
+      // Right side
+      {
+        from: "right_shoulder",
+        to: "right_elbow",
+      },
+      {
+        from: "right_elbow",
+        to: "right_wrist",
+      },
+      {
+        from: "right_shoulder",
+        to: "right_hip",
+      },
+      {
+        from: "right_hip",
+        to: "right_knee",
+      },
+      {
+        from: "right_knee",
+        to: "right_ankle",
+      },
+      // Left side
+      {
+        from: "left_shoulder",
+        to: "left_elbow",
+      },
+      {
+        from: "left_elbow",
+        to: "left_wrist",
+      },
+      {
+        from: "left_shoulder",
+        to: "left_hip",
+      },
+      {
+        from: "left_hip",
+        to: "left_knee",
+      },
+      {
+        from: "left_knee",
+        to: "left_ankle",
+      },
+      // Shoulders and hips
+      {
+        from: "left_shoulder",
+        to: "right_shoulder",
+      },
+      {
+        from: "left_hip",
+        to: "right_hip",
+      },
+      // face
+      {
+        from: "nose",
+        to: "left_eye",
+      },
+      {
+        from: "nose",
+        to: "right_eye",
+      },
+      {
+        from: "nose",
+        to: "left_ear",
+      },
+      {
+        from: "nose",
+        to: "right_ear",
+      },
+      {
+        from: "nose",
+        to: "left_shoulder",
+      },
+      {
+        from: "nose",
+        to: "right_shoulder",
+      },
+    ],
+  };
+};
 
 const CanvasMask = () => {
   const canvasWindow = useRef<HTMLInputElement>(null);
@@ -17,6 +104,7 @@ const CanvasMask = () => {
     croppedImgDimensions,
     setImageUrlPose,
     setImageUrlMask,
+    setPose,
   } = useDrawingStore();
   const {
     tool,
@@ -29,7 +117,7 @@ const CanvasMask = () => {
     setLines,
     setBlackLines,
   } = useMaskingStore();
-  const { isLoading, getMask, getCroppedImage, setMask } = useDrawingApi((err) => {});
+  const { isLoading, getMask, getCroppedImage, getJointLocations, setMask } = useDrawingApi((err) => {});
   const { currentStep, setCurrentStep } = useStepperStore();
 
   /**
@@ -57,6 +145,10 @@ const CanvasMask = () => {
             let imageDataUrl = reader.result;
             setImageUrlPose(imageDataUrl);
           };
+        });
+        getJointLocations(uuid!, (data) => {
+          const mappedPose = mapJointsToPose(data);
+          setPose(mappedPose);
         });
       } catch (error) {
         console.log(error);
