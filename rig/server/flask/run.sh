85de8fe256eb4b742700cacdfc9f8329a3333fdc
EC2 Default User <ec2-user@ip-172-31-28-236.us-east-2.compute.internal>
2021-09-24 19:02:25 +0000

    interm aws updates

--- rig/server/flask/run.sh
+++ rig/server/flask/run.sh
@@ -7,4 +7,4 @@
 ./env.sh
 cp env-config.js static/
 # start flask server
-conda run -n flask gunicorn server:app --access-logfile access.log --log-file error.log -w 2 --threads 2 -b 0.0.0.0:5000 && tail -f error.log
+conda run -n flask gunicorn server:app --access-logfile access.log --log-file error.log -w 2 --threads 10 -b 0.0.0.0:5000 && tail -f error.log
