1f99e7ac1f04389ae32ac779c391500fd559a0be
Somya Jain <somyaj@fb.com>
2021-09-24 14:27:32 -0400

    Added test instructions.

--- torchserve_ap/scripts/docker_test.sh
+++ torchserve_ap/scripts/docker_test.sh
@@ -6,7 +6,7 @@ torchserve --start --model-store model_store --models alphapose.mar --ts-config 
 
 
 cd /home/ap-server/torchserve_ap/scripts
-sleep 3 # wait for torchserve to spin up. 
+sleep 5 # wait for torchserve to spin up. 
 
 ./example_query.sh
 
