530ea8b0a496c8d9a5a1739e187d72dd5a2fb25e
Jesse Smith <jesse.smith@oculus.com>
2021-10-07 13:17:16 -0700

    changed worker and thread count

--- animate/flask/run.sh
+++ animate/flask/run.sh
@@ -5,4 +5,4 @@
 # touch /home/animation-server/animate/flask/error.log
 
 # TODO: following command is failing when run from docker-compose.yaml, but works when run from CL shell inside container
-gunicorn server:app --access-logfile access.log --log-file error.log -w 2 --threads 2 -b 0.0.0.0:5000 ; tail -f error.log
+gunicorn server:app --access-logfile access.log --log-file error.log -w 5 --threads 5 -b 0.0.0.0:5000 ; tail -f error.log
