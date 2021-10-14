0b42f056f036b697fe6494158b019d0f216e9869
s0mya <somyaj@fb.com>
2021-10-13 20:52:13 -0400

    Added soft links to access and error log files.

--- animate/flask/run.sh
+++ animate/flask/run.sh
@@ -4,5 +4,11 @@
 # touch /home/animation-server/animate/flask/access.log
 # touch /home/animation-server/animate/flask/error.log
 
+# As per guidance from https://docs.docker.com/config/containers/logging/
+# and https://github.com/nginxinc/docker-nginx/blob/8921999083def7ba43a06fabd5f80e4406651353/mainline/jessie/Dockerfile#L21-L23
+# forward request and error logs to docker log collector
+ln -sf /dev/stdout access.log \
+	&& ln -sf /dev/stderr error.log
+
 # TODO: following command is failing when run from docker-compose.yaml, but works when run from CL shell inside container
 gunicorn server:app --access-logfile access.log --log-file error.log -w 2 --threads 2 -b 0.0.0.0:5000 ; tail -f error.log
