14e11a38bc8c4e0f1206e4131b6dc91882355998
Sahir Gomez <sahir@fb.com>
2021-10-18 15:38:21 -0400

    added bold to anchor tags in about page

--- ui/www/src/containers/AboutPage.tsx
+++ ui/www/src/containers/AboutPage.tsx
@@ -6,7 +6,7 @@ const AboutPage = () => {
   const history = useHistory();
   return (
     <div className="main-content bg-about" id="about">
-      <div className="home-page">
+      <div className="about-page">
         <Container className="align-content-center">
           <Row className="justify-content-center">
             <Col lg={6} md={10} xs={12}>
@@ -20,8 +20,8 @@ const AboutPage = () => {
               <br />
               <p>
                 {" "}
-                This demo builds upon <a href="https://github.com/facebookresearch/detectron2" target="_blank">Detectron2</a> and <a href="https://github.com/MVIG-SJTU/AlphaPose" target="_blank">AlphaPose</a>.
-                Motion capture data comes from the <a href="http://mocap.cs.cmu.edu/" target="_blank">CMU Graphics Motion Capture Lab</a> and <a href="https://www.mixamo.com/" target="_blank">Mixamo</a>.
+                This demo builds upon <a href="https://github.com/facebookresearch/detectron2" target="_blank" rel="noreferrer" className="bold">Detectron2</a> and <a href="https://github.com/MVIG-SJTU/AlphaPose" target="_blank" rel="noreferrer">AlphaPose</a>.
+                Motion capture data comes from the <a href="http://mocap.cs.cmu.edu/" target="_blank" rel="noreferrer">CMU Graphics Motion Capture Lab</a> and <a href="https://www.mixamo.com/" target="_blank" rel="noreferrer">Mixamo</a>.
               </p>
               <br />
               <p>
