245f1552f2679eb01095e34c3a132bc59242bb04
s0mya <somyaj@fb.com>
2021-11-17 17:08:13 -0500

    reverting dir updates.

--- rig/server/flask/server.py
+++ rig/server/flask/server.py
@@ -16,7 +16,6 @@ import crop_from_bb
 import segment_mask
 import prep_animation_files
 
-
 UPLOAD_FOLDER='./uploads/'
 CONSENT_GIVEN_SAVE_DIR = './consent_given_upload_copies/'
 #VIDEO_SHARE_ROOT='/app/out/public/videos'
@@ -146,9 +145,8 @@ def set_bounding_box_coordinates():
                 )
 
     unique_id = request.form['uuid']
-    work_dir = os.path.join(app.config['UPLOAD_FOLDER'], unique_id)
     bb_path = os.path.join(UPLOAD_FOLDER, unique_id, 'bb.json')
-    if not os.path.exists(work_dir):  # uuid is invalid
+    if not os.path.exists(bb_path):  # uuid is invalid
         return redirect(request.url)
 
     # back up the previous bounding box annotations
@@ -201,7 +199,7 @@ def set_mask():
                 )
     work_dir = os.path.join(app.config['UPLOAD_FOLDER'], request.form['uuid'])
     mask_path = os.path.join(work_dir, 'mask.png')
-    if not os.path.exists(work_dir):
+    if not os.path.exists(mask_path):
         return redirect(request.url)
 
     file = request.files['file']
@@ -243,7 +241,7 @@ def set_joint_locations():
 
     work_dir = os.path.join(app.config['UPLOAD_FOLDER'], request.form['uuid'])
     joint_locations_json_path = os.path.join(work_dir, 'joint_locations.json')
-    if not os.path.exists(work_dir):  # uuid is invalid
+    if not os.path.exists(joint_locations_json_path):  # uuid is invalid
         return redirect(request.url)
 
     # back up the previous joint locations
