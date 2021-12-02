1f4d46ffd2c7a32f765558ae23353c77d376a5ee
Jesse Smith <jesse.smith@oculus.com>
2021-12-02 15:11:42 -0800

    removed commented and unused code

--- rig/server/flask/detect_humanoids.py
+++ rig/server/flask/detect_humanoids.py
@@ -1,12 +1,6 @@
 import os
-import re
-import subprocess, json
-from typing import NewType
 import cv2
-from pathlib import Path
-import numpy as np 
-import boto3
-import base64
+import numpy as np
 import requests
 import json 
 
@@ -14,14 +8,10 @@ import storage_service
 
 interim_store = storage_service.get_interim_store()
 
-
 DETECTRON2_ENDPOINT = os.environ.get("DETECTRON2_ENDPOINT")
 
-def get_bounding_box_from_torchserve_response(response_json, input_img, orig_dims, small_dims, padding=0):
 
-    # f = open('test_log.txt', 'w')
-    # f.write(f'origin dims {orig_dims}\n')
-    # f.write(f'small dims {small_dims}\n')
+def get_bounding_box_from_torchserve_response(response_json, input_img, orig_dims, small_dims, padding=0):
 
     # if prediction fails, make entire image the bounding box
     if 'boxes' not in response_json.keys() or len(response_json['boxes']) == 0:
@@ -37,26 +27,11 @@ def get_bounding_box_from_torchserve_response(response_json, input_img, orig_dim
         x2 = max(x2, _x2)
         y2 = max(y2, _y2)
 
-    # f.write(f'small x1 {x1}\n')
-    # f.write(f'small x2 {x2}\n')
-    # f.write(f'small y1 {y1}\n')
-    # f.write(f'small y2 {y2}\n')
-
     # convert back to image coordinates of the original image
     x1 = int( x1 * orig_dims[1] / small_dims[1])
     x2 = int( x2 * orig_dims[1] / small_dims[1])
     y1 = int( y1 * orig_dims[0] / small_dims[0])
     y2 = int( y2 * orig_dims[0] / small_dims[0])
-
-    # f.write(f'scale_ratio w {orig_dims[1] / small_dims[1]}\n')
-    # f.write(f'scale_ratio h {orig_dims[0] / small_dims[0]}\n')
-
-    # f.write(f' x1 {x1}\n')
-    # f.write(f' x2 {x2}\n')
-    # f.write(f' y1 {y1}\n')
-    # f.write(f' y2 {y2}\n')
-
-    # f.close()
 
     # account for padding
     x1 = max(0, x1 - padding)
@@ -69,17 +44,12 @@ def get_bounding_box_from_torchserve_response(response_json, input_img, orig_dim
 
 def image_resize(unique_id, largest_dim = 400, inter = cv2.INTER_AREA):
     
-    # READ image.png S3 OBJECT as BYTES
-
     image_obj = interim_store.read_bytes(unique_id, 'image.png')
     image = cv2.imdecode(np.asarray(bytearray(image_obj)), cv2.IMREAD_COLOR)
   
     (h, w) = image.shape[:2]
 
-    if h >= w:
-        max_dim = h
-    else:
-        max_dim = w
+    max_dim = max(h, w)
 
     if max_dim <= largest_dim:
         return image, (h, w), (h, w), image
@@ -91,6 +61,7 @@ def image_resize(unique_id, largest_dim = 400, inter = cv2.INTER_AREA):
     resized_img = cv2.resize(image, (reduced_size[1], reduced_size[0]), interpolation = inter)
     
     return resized_img, (h, w), reduced_size, image
+
 
 def detect_humanoids(unique_id):
 
@@ -109,4 +80,3 @@ def detect_humanoids(unique_id):
     
     cropped_img = input_img[bb['y1']:bb['y2'], bb['x1']:bb['x2'], :]
     interim_store.write_bytes(unique_id, 'cropped_image.png', storage_service.np_to_png_bytes(cropped_img))
-
--- rig/server/flask/detect_pose.py
+++ rig/server/flask/detect_pose.py
@@ -6,26 +6,17 @@ import numpy as np
 import cv2
 
 
-
 interim_store = storage_service.get_interim_store()
 
 ALPHAPOSE_ENDPOINT = os.environ.get("ALPHAPOSE_ENDPOINT")
 
 def detect_pose(unique_id):
-    #uuid = work_dir.split('/')[-1]
-    #img_pth = os.path.abspath(os.path.join(work_dir, 'gray_blur.png'))
-    #pull form s3 as bytes
     img_bytes = interim_store.read_bytes(unique_id, 'cropped_image.png')
 
     response_json = requests.post(url=ALPHAPOSE_ENDPOINT, data={'uuid': unique_id, 'image':img_bytes})
 
-    #save alphapose_results.json' under uuid directory in interim bucket
-    # with open(os.path.join(work_dir, 'alphapose_results.json'), 'w') as f:
-    #     json.dump(response_json, f)
-    try:
-        json_object = json.dumps(response_json.json(), indent = 4) 
-    except:
-        assert False  # TODO @Chris - I'm intermittantly getting a 503 error response when querying ALPHAPOSE_ENPOINT
+    json_object = json.dumps(response_json.json(), indent = 4)
+
     interim_store.write_bytes(unique_id, "alphapose_results.json",  bytearray(json_object, "ascii") )
 
     img_np = cv2.imdecode(np.asarray(bytearray(img_bytes)), 1)
@@ -45,11 +36,5 @@ def detect_pose(unique_id):
 
         joint_locations[name] = {'x': x, 'y': y}
 
-
-    #save joint_locations.json to s3
-    joint_locations_s = json.dumps(joint_locations, indent = 4) 
+    joint_locations_s = json.dumps(joint_locations, indent = 4)
     interim_store.write_bytes(unique_id, "joint_locations.json", bytearray(joint_locations_s, "ascii") )
-
-    # output_loc = os.path.join(work_dir, 'joint_locations.json')
-    # with open(output_loc, 'w') as f:
-    #     json.dump(joint_locations, f)
