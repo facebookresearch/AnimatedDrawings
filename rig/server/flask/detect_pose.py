2ae418c1778eebdf4ee15c53673b3e8831ae08f4
Jesse Smith <jesse.smith@oculus.com>
2021-12-02 14:54:31 -0800

    resolved issue #172

--- rig/server/flask/detect_pose.py
+++ rig/server/flask/detect_pose.py
@@ -2,6 +2,8 @@ import os, sys
 import subprocess, json
 import requests
 import storage_service
+import numpy as np
+import cv2
 
 
 
@@ -26,6 +28,8 @@ def detect_pose(unique_id):
         assert False  # TODO @Chris - I'm intermittantly getting a 503 error response when querying ALPHAPOSE_ENPOINT
     interim_store.write_bytes(unique_id, "alphapose_results.json",  bytearray(json_object, "ascii") )
 
+    img_np = cv2.imdecode(np.asarray(bytearray(img_bytes)), 1)
+    h, w = img_np.shape[0:2]
 
     keypoint_names=[
         "nose", "left_eye", "right_eye", "left_ear", "right_ear",
@@ -35,8 +39,11 @@ def detect_pose(unique_id):
     k_iter = iter(response_json.json()['keypoints'])
 
     joint_locations = {}
-    for name, (x, y, _) in zip(keypoint_names, zip(k_iter, k_iter, k_iter)):
-        joint_locations[name] = {'x': int(x), 'y': int(y)}
+    for name, (_x, _y, _) in zip(keypoint_names, zip(k_iter, k_iter, k_iter)):
+        x = int(max(0, min(w, _x)))  # clamp x to int between 0 and img w
+        y = int(max(0, min(h, _y)))  # clamp y to int between 0 and img h
+
+        joint_locations[name] = {'x': x, 'y': y}
 
 
     #save joint_locations.json to s3
