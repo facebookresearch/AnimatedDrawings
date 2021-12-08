c6e822cd508dc3eae28304666047d2057fc1dc78
Sahir Gomez <sahir@fb.com>
2021-12-08 10:05:58 -0500

    implemented function to support pre canned images

--- ui/www/src/components/ExamplesCarousel.tsx
+++ ui/www/src/components/ExamplesCarousel.tsx
@@ -10,20 +10,51 @@ import useStepperStore from "../hooks/useStepperStore";
 
 const ExamplesCarousel = () => {
   const { isLoading, setPreCannedImage } = useDrawingApi((err) => {});
-  const { setUuid } = useDrawingStore();
+  const { setUuid, setDrawing, setOriginalDimensions } = useDrawingStore();
   const { setCurrentStep } = useStepperStore();
 
+  const selectImgFile = (image_name: string) => {
+    switch (image_name) {
+      case "example3.png":
+        return example3;
+      case "example4.jpg":
+        return example4;
+      case "example5.png":
+        return example5;
+      case "example6.png":
+        return example6;
+      default:
+        return example4;
+    }
+  };
+
   /**
-   * Use pre canned image, invokes api to find a predefined 
+   * Use pre canned image, invokes api to find a predefined
    * image stored server-side given its name.
-   * @param image_name 
+   * @param image_name
    */
   const handlePreCannedImage = async (image_name: string) => {
+    let img_file = selectImgFile(image_name);
     try {
+      const res = await fetch(img_file);
+      const blob = await res.blob();
+      const imageDataUrl = URL.createObjectURL(blob);
+      const tempImage = new Image();
+      if (imageDataUrl !== null && imageDataUrl !== undefined)
+        tempImage.src = imageDataUrl;
+      // Calculate the original dimensions of img for the bouding box background.
+      tempImage.onload = function (e) {
+        setOriginalDimensions({
+          width: tempImage.naturalWidth,
+          height: tempImage.naturalHeight,
+        });
+      };
+
       await setPreCannedImage(image_name, (data) => {
         setUuid(data as string);
-        console.log(data);
       });
+
+      setDrawing(imageDataUrl);
       setCurrentStep(2);
     } catch (error) {
       console.log(error);
@@ -61,7 +92,7 @@ const ExamplesCarousel = () => {
           <img
             src={example5}
             alt=""
-            onClick={() => handlePreCannedImage("example4.jpg")}
+            onClick={() => handlePreCannedImage("example5.png")}
           />
         )}
       </div>
