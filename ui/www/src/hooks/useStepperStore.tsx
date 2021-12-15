d0453fd0a48dc2e133d9d40be5ad39d1391af229
Sahir Gomez <sahir@fb.com>
2021-12-15 16:20:18 -0500

    adds error handler to animation step

--- ui/www/src/hooks/useStepperStore.tsx
+++ ui/www/src/hooks/useStepperStore.tsx
@@ -3,15 +3,19 @@ import create from "zustand";
 type StepperState = {
   agreeTerms: boolean | null;
   currentStep: number;
+  errorCode: number | string;
   setCurrentStep: (step: number) => void;
   setAgreeTerms: (agreed: boolean) => void;
+  setError: (code: number | string) => void
 };
 
 const useStepperStore = create<StepperState>((set) => ({
   agreeTerms: null,
   currentStep: 1,
+  errorCode: 0,
   setCurrentStep: (step) => set(() => ({ currentStep: step })),
   setAgreeTerms: (agreed) => set(() => ({ agreeTerms: agreed })),
+  setError: (code) => set(() => ({ errorCode: code })),
 }));
 
 export default useStepperStore;
