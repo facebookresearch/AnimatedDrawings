cc47e291f02d608facc560f56521f12b41bebee5
Jesse Smith <jesse.smith@fb.com>
2022-12-06 14:22:00 -0800

    updated vectors.py to match quaternions.py

--- animate/animator/model/transform.py
+++ animate/animator/model/transform.py
@@ -1,5 +1,6 @@
 import numpy as np
-from model.vector import Vector
+from model.vectors import Vectors
+import logging
 
 
 class Transform():
@@ -9,20 +10,28 @@ class Transform():
         self.transform = np.identity(4)
         self.children = []
 
-    def set_position(self, pos: Vector):
-        self.transform[:-1, -1] = pos.data
+    def set_position(self, pos: Vectors):
+        if pos.vs.shape != (1, 3):
+            logging.critical(f'pos vector must have shape [1,3]. Found: {pos.vs.shape}')
+            assert False
+        self.transform[:-1, -1] = pos.vs
 
-    def look_at(self, fwd: Vector):
+    def look_at(self, fwd: Vectors):
         """Given a forward vector, rotate the transform to face that position without affect scale or translation"""
-        fwd_n: Vector = fwd.copy()
-        fwd_n.norm()
-        tmp: Vector = Vector(0, 1, 0)
-        right: Vector = tmp.cross(fwd)
-        up: Vector = fwd.cross(right)
 
-        self.transform[:-1, 0] = right.data
-        self.transform[:-1, 1] = up.data
-        self.transform[:-1, 2] = fwd.data
+        if fwd.vs.shape != (1, 3):
+            logging.critical(f'look_at fwd vector must have shape [1,3]. Found: {fwd.vs.shape}')
+            assert False
+
+        fwd_n: Vectors = fwd.copy()
+        fwd_n.norm()
+        tmp: Vectors = Vectors([0, 1, 0])
+        right: Vectors = tmp.cross(fwd)
+        up: Vectors = fwd.cross(right)
+
+        self.transform[:-1, 0] = right.vs
+        self.transform[:-1, 1] = up.vs
+        self.transform[:-1, 2] = fwd.vs
 
     def add_child(self, child):
         """Child must be another transform or subclass thereof"""
