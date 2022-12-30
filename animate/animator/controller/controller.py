4991014867f267502299073731ec3f735db7543a
Jesse Smith <jesse.smith@fb.com>
2022-12-30 15:17:11 -0800

    modifications to controller run loops and interactive camera control

--- animate/animator/controller/controller.py
+++ animate/animator/controller/controller.py
@@ -25,8 +25,15 @@ class Controller():
     def set_view(self, view: View):
         self.view = view
 
+    @abstractmethod
     def _tick(self):
-        self.scene.tick()
+        """Subclass and add logic is necessary to progress time"""
+        pass
+    
+    @abstractmethod
+    def _update(self):
+        """Subclass and add logic is necessary to update scene after progressing time"""
+        pass
 
     @abstractmethod
     def _is_run_over(self):
@@ -50,15 +57,29 @@ class Controller():
 
     @abstractmethod
     def _finish_run_loop_iteration(self):
-        """Subclass and add steps necessary before starting next iteration of run loop"""
+        """Subclass and add steps necessary before starting next iteration of run loop. """
+        pass
+
+    @abstractmethod
+    def _prep_for_run_loop(self):
+        """Subclass and add anything necessary to do immediately prior to run loop. """
+        pass
+
+    @abstractmethod
+    def _cleanup_after_run_loop(self):
+        """Subclass and add anything necessary to do after run loop has finished. """
         pass
 
     def run(self):
-        self.scene.initialize_time()
+        """ The run loop. Subclassed controllers should overload and define functionality for each step in this function."""
 
+        self._prep_for_run_loop()
         while not self._is_run_over():
             self._start_run_loop_iteration()
             self._handle_user_input()
             self._tick()
+            self._update()
             self._render()
             self._finish_run_loop_iteration()
+        
+        self._cleanup_after_run_loop()
