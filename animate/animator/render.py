6e2f0ede58c7c505f97c67065b432206ffa01b04
Jesse Smith <jesse.smith@fb.com>
2023-01-12 15:50:36 -0800

    main.py -> render.py, with start() callable by other scripts

--- animate/animator/render.py
+++ animate/animator/render.py
@@ -3,18 +3,13 @@ import logging
 import yaml
 import sys
 from collections import defaultdict
+from pathlib import Path
 
-if __name__ == '__main__':
-    logging.basicConfig(filename='log.txt', level=logging.DEBUG)
 
-    # parse args
-    user_cfg_fn = sys.argv[1]                   # user-specified MVC configs
-    bvh_metadata_cfg_fn = sys.argv[2]           # bvh-specific metadata config
-    char_bvh_retargeting_cfg_fn = sys.argv[3]   # bvh->character retargeting config
-    char_cfg_fn = sys.argv[4]                   # character-specific config
+def start(user_cfg_fn: str, bvh_metadata_cfg_fn: str, char_bvh_retargeting_cfg_fn: str, char_cfg_fn: str):
 
     # create the MVC config by combining base with user-specified options
-    with open('./config/base_cfg.yaml', 'r') as f:
+    with open('./animator/config/base_cfg.yaml', 'r') as f:
         base_cfg = defaultdict(dict, yaml.load(f, Loader=yaml.FullLoader))
     with open(user_cfg_fn, 'r') as f:
         user_cfg = defaultdict(dict, yaml.load(f, Loader=yaml.FullLoader) or {})
@@ -29,6 +24,7 @@ if __name__ == '__main__':
         char_bvh_retargeting_cfg = yaml.load(f, Loader=yaml.FullLoader)
     with open(char_cfg_fn, 'r') as f:
         char_cfg = yaml.load(f, Loader=yaml.FullLoader)
+    char_cfg['char_files_dir'] = str(Path(char_cfg_fn).parent)  # save the path so we can get image and mask from same directory
 
     # create scene
     scene = Scene(cfg['scene'])
@@ -46,7 +42,8 @@ if __name__ == '__main__':
         from controller.video_render_controller import VideoRenderController
         video_fps = 1.0 / bvh_metadata_cfg['frame_time']
         video_frames = bvh_metadata_cfg['frames']
-        controller = VideoRenderController(cfg['controller'], scene, view, video_fps, video_frames)
+        out_dir = str(Path(char_cfg_fn).parent)
+        controller = VideoRenderController(cfg['controller'], scene, view, video_fps, video_frames, out_dir)
     elif cfg['controller']['type'] == 'interactive':
         from animator.controller.interactive_controller import InteractiveController
         controller = InteractiveController(cfg['controller'], scene, view)
@@ -66,7 +63,19 @@ if __name__ == '__main__':
     scene.add_child(ad)
     if cfg['DRAW_AD_RETARGET_BVH']:
         scene.add_child(ad.retargeter.bvh)
-    # ad.offset(Vectors(cfg['CHARACTER_OFFSET']))
 
     # start the run loop
     controller.run()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='log.txt', level=logging.DEBUG)
+
+    user_cfg_fn = sys.argv[1]  # user-specified MVC configs
+    bvh_metadata_cfg_fn = sys.argv[2]  # bvh-specific metadata config
+
+    char_bvh_retargeting_cfg_fn = sys.argv[3]  # bvh->character retargeting config
+
+    char_cfg_fn = sys.argv[4]  # character-specific config
+
+    start(user_cfg_fn, bvh_metadata_cfg_fn, char_bvh_retargeting_cfg_fn, char_cfg_fn)
