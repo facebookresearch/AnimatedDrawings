e4b8d8e7471d8207099e4c9d980ac122d6130986
Jesse Smith <jesse.smith@fb.com>
2023-01-18 13:47:46 -0800

    removed all hardcoded absolute paths from config files

--- animate/animator/render.py
+++ animate/animator/render.py
@@ -1,6 +1,7 @@
 from animator.model.scene import Scene
 from animator.view.view import View
 from animator.controller.controller import Controller
+from animator.utils import resolve_ad_filepath
 import logging
 import yaml
 import sys
@@ -10,19 +11,24 @@ import os
 
 
 def _build_config(user_mvc_cfg_fn: str) -> defaultdict:
-    """ Combines and returns user-specified config file with base config file."""
+    """ Combines and returns user-specified config file with base config file. """
 
-    # create the MVC config by combining base with user-specified options
-    with open(f'{Path(os.environ["AD_ROOT_DIR"],"animate/animator/scene_base_cfg.yaml")}', 'r') as f:
+    # prep the mvc base config
+    with open(f'{Path(os.environ["AD_ROOT_DIR"],"animate/animator/mvc_base_cfg.yaml")}', 'r') as f:
         base_cfg = defaultdict(dict, yaml.load(f, Loader=yaml.FullLoader))
-    with open(user_mvc_cfg_fn, 'r') as f:
+
+    # search for the user-specified mvc confing
+    user_mvc_cfg_p: Path = resolve_ad_filepath(user_mvc_cfg_fn, 'user mvc config')
+    logging.info(f'Using user-specified mvc config file located at {user_mvc_cfg_p.resolve()}')
+
+    with open(str(user_mvc_cfg_p), 'r') as f:
         user_cfg = defaultdict(dict, yaml.load(f, Loader=yaml.FullLoader) or {})
 
+    # build and return config dict
     cfg = defaultdict(dict)
     cfg['scene'] = {**base_cfg['scene'], **user_cfg['scene']}
     cfg['controller'] = {**base_cfg['controller'], **user_cfg['controller']}
     cfg['view'] = {**base_cfg['view'], **user_cfg['view']}
-
     return cfg
 
 
@@ -53,7 +59,7 @@ def start(user_mvc_cfg_fn: str):
 if __name__ == '__main__':
     logging.basicConfig(filename='log.txt', level=logging.DEBUG)
 
-    # user-specified mvc configuration filepath
+    # user-specified mvc configuration filepath. Can be absolute, relative to cwd, or relative to ${AD_ROOT_DIR}
     user_mvc_cfg_fn = sys.argv[1]
 
     start(user_mvc_cfg_fn)
--- animate/animator/utils.py
+++ animate/animator/utils.py
@@ -1,1 +1,21 @@
+from pathlib import Path
+import os
+import logging
+
 TOLERANCE = 10**-5
+
+
+def resolve_ad_filepath(file_name: str, file_type: str) -> Path:
+    """
+    Given input filename, attempts to find the file, first by relative to cwd,
+    then by absolute, the relative to ${AD_ROOT_DIR} enivironmental variable.
+    If not found, prints error message indicating which file_type it is.
+    """
+    if Path(file_name).exists():
+        return Path(file_name)
+    elif Path(os.environ['AD_ROOT_DIR'], file_name).exists():
+        return Path(os.environ['AD_ROOT_DIR'], file_name)
+
+    msg = f'Could not find the {file_type} specified: {file_name}'
+    logging.critical(msg)
+    assert False, msg
