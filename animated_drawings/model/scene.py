e44f3f8a1192c8d6694f593e02483dd591e5d600
Jesse Smith <jesse.smith@fb.com>
2023-02-13 10:52:21 -0800

    added scene

--- animated_drawings/model/scene.py
+++ animated_drawings/model/scene.py
@@ -1,13 +1,10 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
-import yaml
-import logging
 from animated_drawings.model.transform import Transform
 from animated_drawings.model.time_manager import TimeManager
 from animated_drawings.config import SceneConfig
 from animated_drawings.model.floor import Floor
 from animated_drawings.model.animated_drawing import AnimatedDrawing
-from animated_drawings.utils import resolve_ad_filepath
 
 
 class Scene(Transform, TimeManager):
@@ -17,7 +14,7 @@ class Scene(Transform, TimeManager):
     It keeps track of global time.
     """
 
-    def __init__(self, cfg: SceneConfig):
+    def __init__(self, cfg: SceneConfig) -> None:
         """ Takes in the scene dictionary from an mvc config file and prepares the scene. """
         super().__init__()
 
@@ -35,41 +32,6 @@ class Scene(Transform, TimeManager):
             if cfg.add_ad_retarget_bvh:
                 self.add_child(ad.retargeter.bvh)
             
-            continue
-
-            # get filenames from config
-            motion_cfg_fn: str = each['motion_cfg']
-            retarget_cfg_fn: str = each['retarget_cfg']
-            #character_cfg_fn: str = each['character_cfg']
-
-            # resolves paths
-            motion_cfg_p = resolve_ad_filepath(motion_cfg_fn, 'motion cfg')
-            retarget_cfg_p = resolve_ad_filepath(retarget_cfg_fn, 'retarget cfg')
-
-            # log what we're doing
-            logging.info(f'Using motion_cfg located at {motion_cfg_p.resolve()}')
-            logging.info(f'Using retarget_cfg located at {retarget_cfg_p.resolve()}')
-            logging.info(f'Using character_cfg located at {character_cfg_p.resolve()}')
-
-            # load the configs
-            with open(str(motion_cfg_p), 'r') as f:
-                motion_cfg = yaml.load(f, Loader=yaml.FullLoader)
-            with open(str(retarget_cfg_p), 'r') as f:
-                retarget_cfg = yaml.load(f, Loader=yaml.FullLoader)
-            #with open(str(character_cfg_p), 'r') as f:
-            #    char_cfg = yaml.load(f, Loader=yaml.FullLoader)
-
-            # save the path of parent dir so we can get image and mask from same directory
-            #char_cfg['char_files_dir'] = str(character_cfg_p.parent)
-
-            # add the character to the scene
-            ad = AnimatedDrawing(char_cfg, retarget_cfg, motion_cfg)
-            self.add_child(ad)
-
-            # add bvh to the scene if we're going to visualize it
-            if cfg.add_ad_retarget_bvh:
-                self.add_child(ad.retargeter.bvh)
-
     def progress_time(self, delta_t: float) -> None:
         """
         Entry point called to update time in the scene by delta_t seconds.
