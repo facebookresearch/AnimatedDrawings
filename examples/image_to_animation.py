18542e3dc7c41fcaa6e487b51d311a8bf75aa577
Jesse Smith <jesse.smith@fb.com>
2023-04-04 14:53:54 -0700

    switched to MIT license

--- examples/image_to_animation.py
+++ examples/image_to_animation.py
@@ -1,4 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 from image_to_annotations import image_to_annotations
 from annotations_to_animation import annotations_to_animation
--- tests/test_render.py
+++ tests/test_render.py
@@ -1,4 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 from animated_drawings import render
 from pkg_resources import resource_filename
