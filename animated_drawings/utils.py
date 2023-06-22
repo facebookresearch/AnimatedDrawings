7fd149e20da8ccae2db1e63256dfe710432fde64
Urjit Singh Bhatia <urjitsb87@gmail.com>
2023-06-21 18:31:36 -0700

    remove accidental unused import

--- animated_drawings/utils.py
+++ animated_drawings/utils.py
@@ -1,7 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-import pathlib
 
 from PIL import Image, ImageOps
 import numpy as np
